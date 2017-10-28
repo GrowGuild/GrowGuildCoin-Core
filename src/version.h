c2eb47e06080ebadbdda6929e5c611f82428275b
GGCDMBit <growguildcoind@gmail.com>
2017-10-28 15:34:19 -0700

    Update version.h

--- src/version.h
+++ src/version.h
@@ -43,8 +43,8 @@ static const int MIN_PEER_PROTO_VERSION = 60016;
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOkeKS_VERSION_START = 60002;
-static const int NOkeKS_VERSION_END = 60006;
+static const int NOGGCS_VERSION_START = 60002;
+static const int NOGGCS_VERSION_END = 60006;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
